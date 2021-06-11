da3f2d76eaceffdcb476046a642c6d15d98de7b5
Manish Meganathan <757manish@gmail.com>
2021-06-11 14:28:41 +0530

    Add token module
    - Added a package lexer and a file token.go that handles the lexological tokens used by the interpreter

---
+++ lexer/token.go
@@ -0,0 +1,82 @@
+package lexer
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Arithmetic Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	// Logical Operators
+	LT     = "<"
+	GT     = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+// Language keyword mapper
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+// A type alias that represents the type of a token
+type TokenType string
+
+// A structure that represents a Lexilogical token
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// A constructor function that generates and returns a new
+// Token object for a given token type and a character byte
+func NewToken(tokenType TokenType, ch byte) Token {
+	// Generate and return the character as a token object
+	return Token{Type: tokenType, Literal: string(ch)}
+}
+
+// A function that looks up the token type for
+// a given identifier and returns it.
+func LookUpIndentifier(ident string) TokenType {
+	// Retrieve the token type for the
+	// ident from the map of keywords
+	if tok, ok := keywords[ident]; ok {
+		// Return the user defined identifier
+		return tok
+	}
+
+	// Return the reserved identifier
+	return IDENT
+}
