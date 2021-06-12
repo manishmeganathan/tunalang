30e29fbf227337edaf37ca93b99909c3d6eae769
Manish Meganathan <757manish@gmail.com>
2021-06-12 13:15:30 +0530

    Add parser errors
    - Added a file parsers.go and moved the ParseProgram, parseStatement and parseLetStatement methods to it
    - Added parsers error functionality. New field in the Parser struct to collect errors, a new method to add peek errors
    - Updated test suite to check for parse errors

--- parser/parser.go
+++ parser/parser.go
@@ -1,14 +1,18 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/manishmeganathan/tuna/lexer"
-	"github.com/manishmeganathan/tuna/syntaxtree"
 )
 
 // A structure that represents a Parser
 type Parser struct {
 	// Represents the lexer used by the parser
 	Lexer *lexer.Lexer
+
+	// Represent the errors collected by the parser
+	Errors []string
 
 	// Represents the current token on the parser queue
 	cursorToken lexer.Token
@@ -22,7 +26,7 @@ type Parser struct {
 // such that both the cursor and peek tokens are set
 func NewParser(l *lexer.Lexer) *Parser {
 	// Construct a parser with the lexer
-	p := &Parser{Lexer: l}
+	p := &Parser{Lexer: l, Errors: make([]string, 0)}
 
 	// Advance two tokens such that cursorToken
 	// and peekToken are both set
@@ -39,79 +43,6 @@ func (p *Parser) NextToken() {
 	p.cursorToken = p.peekToken
 	// Advance the peek to the next lexer token
 	p.peekToken = p.Lexer.NextToken()
-}
-
-// A method of Parser that parses the lexer input
-// into an abstract syntax tree Program
-func (p *Parser) ParseProgram() *syntaxtree.Program {
-	// Create a syntax tree program
-	program := &syntaxtree.Program{}
-	// Initialize the syntax tree statements slice
-	program.Statements = []syntaxtree.Statement{}
-
-	// Iterate until the lexer returns an EOF token
-	for !p.isCursorToken(lexer.EOF) {
-		// Parse the current token into a statement
-		stmt := p.parseStatement()
-		// Check if the statement exists
-		if stmt != nil {
-			// Add the statement to the syntax tree program statements
-			program.Statements = append(program.Statements, stmt)
-		}
-
-		// Advance the parse cursor
-		p.NextToken()
-	}
-
-	// Return the parsed syntax tree program
-	return program
-}
-
-// A method of Parser that parses the token in the
-// parse cursor into an syntax tree statement node
-func (p *Parser) parseStatement() syntaxtree.Statement {
-	// Check the value of the token in the parse cursor
-	switch p.cursorToken.Type {
-	// Let Statement
-	case lexer.LET:
-		// Parse the statement into a 'let' statement
-		return p.parseLetStatement()
-
-	// Default Case (not a recognized statement)
-	default:
-		return nil
-	}
-}
-
-// A method of Parser that parses the token in the parse
-// cursor into a LET statement node for the syntax tree
-func (p *Parser) parseLetStatement() *syntaxtree.LetStatement {
-	// Create a LET statement node with the token
-	stmt := &syntaxtree.LetStatement{Token: p.cursorToken}
-
-	// Check the peek cursor for an identfier token and move to it
-	if !p.expectPeek(lexer.IDENT) {
-		// no identifier token detected i.e invalid let statement
-		return nil
-	}
-
-	// Assign the statement identifier to the statement node
-	stmt.Name = &syntaxtree.Identifier{Token: p.cursorToken, Value: p.cursorToken.Literal}
-
-	// Check the peek cursor for assignment token and move to it
-	if !p.expectPeek(lexer.ASSIGN) {
-		// no assign token detcted i.e invalid let statement
-		return nil
-	}
-
-	// Advance until semicolon in encountered (TODO: let statement value detection)
-	for !p.isCursorToken(lexer.SEMICOLON) {
-		// Advance the parse cursor
-		p.NextToken()
-	}
-
-	// Return the parsed let statement
-	return stmt
 }
 
 // A method of Parser that checks if the parse
@@ -136,7 +67,14 @@ func (p *Parser) expectPeek(t lexer.TokenType) bool {
 		// Return true
 		return true
 	} else {
-		// no match - return false
+		// Add a peer error to the parser
+		p.peekError(t)
+		// Return false
 		return false
 	}
+}
+
+func (p *Parser) peekError(t lexer.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.Errors = append(p.Errors, msg)
 }
