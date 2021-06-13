0284901b7e7311f43760695c50b295497222a60a
Manish Meganathan <757manish@gmail.com>
2021-06-13 19:43:20 +0530

    Add infix expression node to the syntax tree

--- syntaxtree/expressions.go
+++ syntaxtree/expressions.go
@@ -8,7 +8,7 @@ import (
 
 // A structure that represents a prefix expression node on the syntax tree
 type PrefixExpression struct {
-	// Represents the prefix token
+	// Represents the prefix operator token
 	Token lexer.Token
 
 	// Represents the operator literal string
@@ -31,9 +31,48 @@ func (pe *PrefixExpression) String() string {
 
 	// Start expression with parenthesis
 	out.WriteString("(")
-	// Add expression operator and expression string value
+	// Add the operator and expression string values
 	out.WriteString(pe.Operator)
 	out.WriteString(pe.Right.String())
+	// End expression with parenthesis
+	out.WriteString(")")
+
+	// Return the string of the buffer
+	return out.String()
+}
+
+// A structure that represents an infix expression node on the syntax tree
+type InfixExpression struct {
+	// Represents the infix operator token
+	Token lexer.Token
+
+	// Represents the expression before the operator
+	Left Expression
+
+	// Represents the operator literal string
+	Operator string
+
+	// Represents the expression after the operator
+	Right Expression
+}
+
+// A method of InfixExpression to satisfy the Expression interface
+func (pe *InfixExpression) expressionNode() {}
+
+// A method of InfixExpression that returns its token literal value
+func (pe *InfixExpression) TokenLiteral() string { return pe.Token.Literal }
+
+// A method of InfixExpression that returns its string representation
+func (ie *InfixExpression) String() string {
+	// Declare a bytes buffer
+	var out bytes.Buffer
+
+	// Start expression with parenthesis
+	out.WriteString("(")
+	// Add left expression, the operator and the right expression values
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
 	// End expression with parenthesis
 	out.WriteString(")")
 
