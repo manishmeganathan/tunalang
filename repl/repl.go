94b799bd2139f51c515ceabf1a8fc39f40f8771f
Manish Meganathan <meganathan.manish@gmail.com>
2021-06-22 01:22:59 +0530

    Integrate Evaluator into the REPL
    - Integrated the basic evaluator into the REPL (finally an actual REPL)
    - Added some code documentation

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/manishmeganathan/tuna/evaluator"
 	"github.com/manishmeganathan/tuna/lexer"
 	"github.com/manishmeganathan/tuna/parser"
 )
@@ -50,22 +51,33 @@ func StartREPL(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 		// Create a Lexer instance
 		lex := lexer.NewLexer(line)
+		// Create a Parser instance
 		par := parser.NewParser(lex)
 
+		// Parse the input into a Program
 		program := par.ParseProgram()
+		// Check for parser errors
 		if len(par.Errors) != 0 {
 			printParserErrors(out, par.Errors)
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		// Evaluate the Program
+		evaluated := evaluator.Evaluate(program)
+		// Print the evaluated values if they exist
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
 func printParserErrors(out io.Writer, errors []string) {
+	// Print some error header
 	io.WriteString(out, "Woops! We has some trouble parsing!\n")
 	io.WriteString(out, "parser errors:\n")
+
+	// Iterate over the parser errors and print them out
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
