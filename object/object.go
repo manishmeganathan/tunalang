0c5659ff29d41e46532175296c1ff848e2416b07
Manish Meganathan <meganathan.manish@gmail.com>
2021-06-22 10:39:45 +0530

    Add Return objects
    - Added a struct ReturnValue to the object pkg. This struct implements the object interface

--- object/object.go
+++ object/object.go
@@ -3,6 +3,8 @@ package object
 const (
 	NULL_OBJ = "NULL"
 
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
 )
@@ -15,3 +17,15 @@ type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+// A structure that represents a Returned object
+type ReturnValue struct {
+	// Represents the returned object
+	Value Object
+}
+
+// A method of ReturnValue that returns the Return value type
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+
+// A method of ReturnValue that returns the string value of the Returned object
+func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
