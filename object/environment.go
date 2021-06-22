01db37e6b414c2717f5b220d73915277bd1dacd6
Manish Meganathan <meganathan.manish@gmail.com>
2021-06-22 13:46:06 +0530

    Add scoped environments
    - Added a constructor for enclosed environments
    - Added a field outer for representing the root environment (outer scope)
    - Extended the Get method to retrieve data from the outer scope if not found in the main store

--- object/environment.go
+++ object/environment.go
@@ -4,6 +4,8 @@ package object
 type Environment struct {
 	// Represents the memory pool of stored objects
 	store map[string]Object
+	// Represents the outer environment scope
+	outer *Environment
 }
 
 // A constructor function that generates
@@ -12,13 +14,32 @@ func NewEnvironment() *Environment {
 	// Initialize the store hash map
 	s := make(map[string]Object)
 	// Return the environment
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
+}
+
+// A constructor function that generates and returns
+// an enclosed Environment given the outer environment
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	// Create a new environment
+	env := NewEnvironment()
+	// Assign its outer field to the given outer scope
+	env.outer = outer
+	// Return the new enclosed environment
+	return env
 }
 
 // A method of Environment to retrieve a value from the store
 func (e *Environment) Get(name string) (Object, bool) {
 	// Retrieve the value from the store
 	obj, ok := e.store[name]
+
+	// Check if an outer environment exists
+	// if the value is not found
+	if !ok && e.outer != nil {
+		// Retrive the value from the outer environment
+		obj, ok = e.outer.Get(name)
+	}
+
 	// Return the object and the ok flag
 	return obj, ok
 }
