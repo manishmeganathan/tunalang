7e5b4a88aae933f27a696b7d33305a61a394d4d0
Manish Meganathan <meganathan.manish@gmail.com>
2021-06-22 11:14:47 +0530

    Add Error objects
    - Added an Error struct and methods to implement the Object interface
    - Added a constructor for the Error struct

--- object/object.go
+++ object/object.go
@@ -1,9 +1,12 @@
 package object
 
+import "fmt"
+
 const (
 	NULL_OBJ = "NULL"
 
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ        = "ERROR"
 
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
@@ -29,3 +32,21 @@ func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 
 // A method of ReturnValue that returns the string value of the Returned object
 func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
+
+// A structure that represents an Error object
+type Error struct {
+	// Represents the error message
+	Message string
+}
+
+// A method of Error that returns the Error value type
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+
+// A method of Error that returns the string value of the Error object
+func (e *Error) Inspect() string { return "ERROR: " + e.Message }
+
+// A constructor function that generates and returns a new Error
+// for a given message and some variadic interface
+func NewError(format string, a ...interface{}) *Error {
+	return &Error{Message: fmt.Sprintf(format, a...)}
+}
