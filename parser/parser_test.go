9d2296879c4e97953e24bd6a147fd899cefcef75
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-19 11:53:42 +0530

    Add list literal parsing tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -620,6 +620,34 @@ func TestCallExpressionParameterParsing(t *testing.T) {
 	}
 }
 
+func TestParsingArrayLiterals(t *testing.T) {
+	input := "[1, 2 * 2, 3 + 3]"
+
+	l := lexer.NewLexer(input)
+	p := NewParser(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt, ok := program.Statements[0].(*syntaxtree.ExpressionStatement)
+	if !ok {
+		t.Fatalf("statement not syntaxtree.ExpressionStatement. got=%T", stmt.Expression)
+	}
+
+	array, ok := stmt.Expression.(*syntaxtree.ListLiteral)
+	if !ok {
+		t.Fatalf("exp not syntaxtree.ArrayLiteral. got=%T", stmt.Expression)
+	}
+
+	if len(array.Elements) != 3 {
+		t.Fatalf("len(array.Elements) not 3. got=%d", len(array.Elements))
+	}
+
+	testIntegerLiteral(t, array.Elements[0], 1)
+	testInfixExpression(t, array.Elements[1], 2, "*", 2)
+	testInfixExpression(t, array.Elements[2], 3, "+", 3)
+}
+
 func testLetStatement(t *testing.T, s syntaxtree.Statement, name string) bool {
 
 	if s.TokenLiteral() != "let" {
