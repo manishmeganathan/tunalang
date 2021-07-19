1ab51ea6d3ea670565d23c853d88b32e7bc57a67
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-20 01:00:06 +0530

    Add HashKey method for base data types
    - Added the HashKey method for Integer, String and Boolean data types

--- object/datatypes.go
+++ object/datatypes.go
@@ -1,6 +1,9 @@
 package object
 
-import "fmt"
+import (
+	"fmt"
+	"hash/fnv"
+)
 
 // A structure that represents a Null object
 type Null struct{}
@@ -23,6 +26,12 @@ func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 // A method of Integer that returns the string value of the Integer
 func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
 
+// A method of Integer that return the HashKey of the object
+func (i *Integer) HashKey() HashKey {
+	// Create and return the HashKey object from the integer value
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
 // A structure that represents a Boolean object
 type Boolean struct {
 	// Represents the value of the Boolean
@@ -35,6 +44,24 @@ func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 // A method of Boolean that returns the string value of the Boolean
 func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
 
+// A method of Boolean that returns the HashKey of the object
+func (b *Boolean) HashKey() HashKey {
+	// Declare an unsigned int64
+	var value uint64
+
+	if b.Value {
+		// If the Boolean is true, set the value to 1
+		value = 1
+
+	} else {
+		// Else set the value to 0
+		value = 0
+	}
+
+	// Create and return the HashKey object
+	return HashKey{Type: b.Type(), Value: value}
+}
+
 // A structure that represents a String object
 type String struct {
 	// Represents the value of the String
@@ -46,3 +73,14 @@ func (s *String) Type() ObjectType { return STRING_OBJ }
 
 // A method of String that returns the string value of the String
 func (s *String) Inspect() string { return s.Value }
+
+// A method of String that returns the HashKey of the object
+func (s *String) HashKey() HashKey {
+	// Create new 64bit FNV hasher
+	h := fnv.New64a()
+	// Write the string value to the hasher
+	h.Write([]byte(s.Value))
+
+	// Return the HashKey object
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
