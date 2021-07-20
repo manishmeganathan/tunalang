652588f154eb7c026b574106d227d95d304e3d64
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-20 18:32:56 +0530

    Create new repl module
    - Added a file figlet.go for storing REPL figlets
    - Moved the figlets from repl.go

---
+++ repl/figlet.go
@@ -0,0 +1,23 @@
+package repl
+
+const TUNA = `
+oooooooooooooooooooooooooooooooooo
+  dP
+  88
+d8888P dP    dP 88d888b. .d8888b.
+  88   88    88 88'  '88 88'  '88
+  88   88.  .88 88    88 88.  .88
+  '88P '88888P' db    db '8888888.
+oooooooooooooooooooooooooooooooooo
+`
+
+const TUNA2 = `
+  dP                              dP                            
+  88                              88                            
+d8888P dP    dP 88d888b. .d8888b. 88 .d8888b. 88d888b. .d8888b. 
+  88   88    88 88'  '88 88'  '88 88 88'  '88 88'  '88 88'  '88 
+  88   88.  .88 88    88 88.  .88 88 88.  .88 88    88 88.  .88 
+  '88P '88888P' db    db '88888P8 db '88888P8 db    db '8888P88 
+oooooooooooooooooooooooooooooooooooooooooooooooooooooooo~~~~.88
+							d8888P 
+`
--- repl/repl.go
+++ repl/repl.go
@@ -12,27 +12,6 @@ import (
 )
 
 const PROMPT = ">> "
-const TUNA = `
-oooooooooooooooooooooooooooooooooo
-  dP
-  88
-d8888P dP    dP 88d888b. .d8888b.
-  88   88    88 88'  '88 88'  '88
-  88   88.  .88 88    88 88.  .88
-  '88P '88888P' db    db '8888888.
-oooooooooooooooooooooooooooooooooo
-`
-
-const TUNA2 = `
-  dP                              dP                            
-  88                              88                            
-d8888P dP    dP 88d888b. .d8888b. 88 .d8888b. 88d888b. .d8888b. 
-  88   88    88 88'  '88 88'  '88 88 88'  '88 88'  '88 88'  '88 
-  88   88.  .88 88    88 88.  .88 88 88.  .88 88    88 88.  .88 
-  '88P '88888P' db    db '88888P8 db '88888P8 db    db '8888P88 
-oooooooooooooooooooooooooooooooooooooooooooooooooooooooo~~~~.88
-							d8888P 
-`
 
 // A function that starts the Tuna REPL
 func StartREPL(in io.Reader, out io.Writer) {
