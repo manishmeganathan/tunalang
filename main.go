af164c5c978564d8b519c6fa96401b3cdc030850
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-20 18:33:55 +0530

    Update main.go for v1.0

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 	"github.com/manishmeganathan/tunalang/repl"
 )
 
-const version = "v0.7.0"
+const version = "v1.0.0"
 
 func main() {
 	fmt.Println(repl.TUNA2)
 	fmt.Printf("The Tuna Programming Language %s [%s-%s].\n", version, strings.Title(runtime.GOOS), strings.ToUpper(runtime.GOARCH))
-	fmt.Println("Welcome to the Tuna REPL. Visit www.github.com/manishmeganathan/tuna for more information.")
+	fmt.Println("Welcome to the Tuna REPL. Visit www.github.com/manishmeganathan/tunalang for more information.")
 	repl.StartREPL(os.Stdin, os.Stdout)
 }
